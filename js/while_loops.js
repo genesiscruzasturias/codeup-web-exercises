4bfa1435c854ea438bce3baecc7cd36215a1921a
Genesis Cruz <genesis.cruzasturias@gmail.com>
2023-08-11 15:32:54 -0500

    commit these new changes with semantic HTML

--- js/while_loops.js
+++ js/while_loops.js
@@ -1,40 +1,58 @@
 console.log("Hello!");
 
 //While Loop
-let i = 2, j = 65536;
-while (i <= j) {
-    console.log(i);
-    i*=2;
-}
+// let i = 2, j = 65536;
+// while (i <= j) {
+//     console.log(i);
+//     i*=2;
+// }
 
 //Do While Loop
 //generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop
 //inside of the loop your code should generate another random number between 1 and 5, simulating the amount of cones being bought by her clients.
 //Use a do-while loop to log to the console the amount of cones sold to each person.
 
-let allCones = Math.floor(Math.random() * 50) + 50;
-
-
-do {
-    Math.floor(Math.random() * 5) + 1;
-} while()
-
-let totalCones = getRandomInt(50, 100);
-let wanttoBuy;
-
-do {
-    wanttoBuy = getRandomInt(1, 5);
-
-    if(wanttoBuy < totalCones){
-        console.log("Customer wants to buy" + wanttoBuy)
-    } else {
-
-    }
-    Math.floor(Math.random() * 5) + 1;
-}while (totalCones >= 0);
+// let allCones = Math.floor(Math.random() * 50) + 50;
+//
+//
+// do {
+//     Math.floor(Math.random() * 5) + 1;
+// } while()
+//
+// let totalCones = getRandomInt(50, 100);
+// let wanttoBuy;
+//
+// do {
+//     wanttoBuy = getRandomInt(1, 5);
+//
+//     if(wanttoBuy < totalCones){
+//         console.log("Customer wants to buy" + wanttoBuy)
+//     } else {
+//
+//     }
+//     Math.floor(Math.random() * 5) + 1;
+// }while (totalCones >= 0);
 
 //generates a random number between 50 and 100 representing the amount of cones to sell before you start your loop
 //inside of the loop your code should generate another random number between 1 and 5,simulating the amount of cones being bought by her clients
 //Use a do-while loop to log to the console the amount of cones sold to each person.
 
-l
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+do {
+    let conesBought = Math.floor(Math.random() * 5) + 1;
+} while (true);
+
+let totalCones = Math.floor(Math.random() * 50) + 50;
+
+do {
+    if (conesBought < totalCones) {
+        console.log("Client wants " + conesBought)
+    } else if (allCones === totalCones) {
+        console.log("I sold all my cones!")
+    } if (conesBought > 5) {
+        console.log("I dont have enough cones!")
+    } else {
+        console.log("Idk yet.")
+    }
+}while (totalCones >=0)
