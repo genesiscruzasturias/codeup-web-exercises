448d7edba6b1ed274e1dcec629dad3e968295add
Genesis Cruz <genesis.cruzasturias@gmail.com>
2023-09-06 11:16:06 -0500

    commit

---
+++ js/jquery.js
@@ -0,0 +1,5 @@
+console.log("hello")
+
+$(function() {
+    alert( 'Here is my awesome alert!' );
+});
---
+++ js/js-assessment-prep.js
@@ -0,0 +1,11 @@
+console.log("hi")
+
+function isPositive (number) {
+    if (Number(number) > 0) {
+        return number + true;
+    } else {
+        return false;
+    }
+}
+isPositive(3);
+
