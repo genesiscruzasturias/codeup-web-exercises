c80b4e7f0d5a8f288b35ecd3d8bd7c6170d4d138
Genesis Cruz <genesis.cruzasturias@gmail.com>
2023-09-06 14:02:30 -0500

    commit

---
+++ js/jquery-js.js
@@ -0,0 +1,9 @@
+$(function() {
+    alert( 'Here is my awesome alert! Dom has finished loading!' );
+});
+
+console.log(($("p")));
+
+console.log($("#ice-cream-list"));
+
+console.log($(".list-item"))
--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,29 @@
 console.log("hello")
 
-$(function() {
-    alert( 'Here is my awesome alert! Dom has finished loading!' );
-});
+// Was for previous exercise
+// $(function() {
+//     alert( 'Here is my awesome alert! Dom has finished loading!' );
+// });
+
+// console.log($(".list-item"))
+//
+// var contents = $('#paragraph').html();
+// alert(contents);
+//
+// // creating border around .codeup
+// $('.codeup').css('border', '1px solid red')
+
+//set li font size
+$('li').css('font-size', '20px');
+
+//highlight elements
+// $('p').css('background-color', '#FF0');
+// $('li').css('background-color', '#FF0');
+// $('h1').css('background-color', '#FF0');
+
+//alert
+var contents2 = $('h1').html();
+alert(contents2);
+
+//multiple
+$('p, li, h1').css('background-color', '#FF0');
