0a7bc1255380426c387c8fe6030adb2ec2d2723e
Genesis Cruz <genesis.cruzasturias@gmail.com>
2023-11-05 21:17:58 -0600

    commit:

--- js/weather-map.js
+++ js/weather-map.js
@@ -37,164 +37,146 @@ const updateButton = document.getElementById('update-forecast-btn');
 
 // NEW CITY 5 DAY FORECAST/CURRENT FORECAST
 function updateForecast() {
-    // GRABS THE CITY THE USER INPUT INTO THE TEXT FIELD PROVIDED
     const cityInput = document.getElementById('mapbox-search').value;
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${keys.weatherMap}&units=imperial`;
-    const lngLat = marker.getLngLat();
-    // GROUPS THE FORECAST DATA DAY BY DAY
-    fetch(BASE_FORECAST_URL + `q=${cityInput}&appid=${keys.weatherMap}&units=imperial`)
-        // CONVERTS API INTO JSON FORMAT
+
+    fetch(apiUrl)
         .then(response => response.json())
-        // CONTAINS DATA FOR THE SPECIFIED CITY
-        .then(list => {
-            // CHECK WHERE THE PIN IS AT
-            // console.log(list.city.coord)
-            marker.setLngLat(list.city.coord)
-            const lngLatArr= Object.values(list.city.coord);
-            console.log(lngLatArr)
+        .then(data => {
+            // Call the updateCurrentCityForecast function with the retrieved data
+            updateCurrentCityForecast(data);
+
+            const { lat, lon } = data.coord;
             map.flyTo({
-                center: [lngLatArr[1], lngLatArr[0]],
+                center: [lon, lat],
                 essential: true,
                 zoom: 13
-            })
-            //DIDNT UNDERSTAND IT BUT KNOW IT KIND OF WORKS
-            const forecastItems = list.list.slice(0, 80); // Get the necessary forecast items
-            // CLEARS "insert-forecast" element
-            forecastElement.innerHTML = ''; // Clear previous forecast
+            });
+            if (marker) {
+                marker.remove();
+            }
+            marker.setLngLat([lon, lat]).addTo(map);
+            // Fetch forecast data for the searched city
+            const forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=${keys.weatherMap}&units=imperial`;
+            return fetch(forecastApiUrl);
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Update the 5-day forecast with the retrieved data
+            let html = '';
 
-            const forecastByDay = {};
-            forecastItems.forEach(item => {
-                const date = new Date(item.dt * 1000);
+            for (let i = 0; i < 5; i++) {
+                const forecastData = data.list[i * 8];
+                const date = new Date(forecastData.dt * 1000);
                 const dayOfWeek = date.toLocaleDateString('en-US', { weekday: 'long' });
+                const weatherIconUrl = `https://openweathermap.org/img/wn/${forecastData.weather[0].icon}@2x.png`;
 
-                // Group forecast items by day
-                if (!forecastByDay[dayOfWeek]) {
-                    forecastByDay[dayOfWeek] = item;
-                }
-            });
+                html += `
+<div className="card" style="width: 18rem;">
+<ul className="list-group list-group-flush">
+<li className="list-group-item">${dayOfWeek}</li>
+<li className="list-group-item">Main: ${forecastData.main.temp.toFixed(0)}°F</li>
+<li className="list-group-item">Low: ${forecastData.main.temp_min}°F</li>
+<li className="list-group-item">${forecastData.weather[0].main}</li>
+<li className="list-group-item"><img src="${weatherIconUrl}" alt="weather-icon"></li>
+</ul>
+</div>`;
+            }
 
-            // Display the 5-day forecast
-            Object.keys(forecastByDay).forEach(dayOfWeek => {
-                const item = forecastByDay[dayOfWeek];
-                const forecastTemp = item.main.temp.toFixed(0);
-                const tempLow = item.main.temp_min.toFixed(0);
-                const forecastConditions = item.weather[0].main;
-                const iconUrl = 'https://openweathermap.org/img/wn/10d@2x.png'
-                const icon = item.weather[0].icon
-
-                const forecastItem = document.createElement('div');
-                forecastElement.classList.add('forecast-div-two')
-
-
-                forecastItem.innerHTML = `
-                <div class="day-week">${dayOfWeek}</div>
-                <hr>
-                <div class="icon"></div>
-                <div>Main: ${forecastTemp}°F</div>
-                <hr>
-                <div>Low: ${tempLow}°F</div>
-                <br>
-                <div>${forecastConditions}</div>
-            
-            `;
-
-                forecastElement.appendChild(forecastItem);
-
-
-            });
-
-            // API request to fetch current city
-            fetch(apiUrl)
-                .then(response => response.json())
-                .then(data => {
-                    const currentCity = data.name;
-                    const currentTemp = data.main.temp.toFixed(0);
-                    const currentHumidity = data.main.humidity;
-                    const currentConditions = data.weather[0].main;
-
-
-                    currentCityElement.innerHTML = `
-                    
-                    <div class="city-name">${data.name}</div>
-                    <div>Current Temp:</div> 
-                    <div>${data.main.temp.toFixed(0)}°F</div>
-                    <div>Humidity:</div>
-                    <div>${data.main.humidity}%</div>
-                    <div>Conditions:</div> 
-                    <div class="cloud-img">${data.weather[0].main}</div>
-                    `;
-
-                })
+            // Insert the forecast data into the element with id 'insert-forecast'
+            document.getElementById('insert-forecast').innerHTML = html;
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
         });
+    // Add an event listener to the input box to handle Enter key press event
+    const searchInput = document.getElementById('mapbox-search');
+    searchInput.addEventListener('keydown', event => {
+        if (event.key === 'Enter') {
+            const cityInput = searchInput.value;
+            updateForecast(cityInput);
+        }
+    });
 }
+
+// Add an event listener to the search button to trigger the updateForecast function
+const searchButton = document.getElementById('update-forecast-btn');
+searchButton.addEventListener('click', updateForecast);
 
         updateButton.addEventListener('click', updateForecast);
 
 const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 const BASE_FORECAST_URL = 'https://api.openweathermap.org/data/2.5/forecast?'
-// JUST TO MAKE SURE I WAS RIGHT ON TRACK
-// console.log(BASE_WEATHER_URL + `lat=${47.60537214369371}&lon=${-122.32423484983421}&appid=${WEATHER_MAP_KEY}&units=imperial`)
+// Fetch forecast data using fetch API
+fetch(`${BASE_FORECAST_URL}lat=${47.60537214369371}&lon=${-122.32423484983421}&appid=${keys.weatherMap}&units=imperial`)
+    .then(response => response.json())
+    .then(data => {
+        // console.log(data.list)
+        let html = '';
 
-// SEATTLE 5 DAY FORECAST
-$.get(BASE_FORECAST_URL + `lat=${47.60537214369371}&lon=${-122.32423484983421}&appid=${keys.weatherMap}&units=imperial`).done((list) => {
+        for (let i = 0; i < 5; i++) {
+            const forecastData = data.list[i * 8];
+            // console.log(data.list[i*8])
+            const date = new Date(forecastData.dt * 1000);
+            // console.log(date)
+            const dayOfWeek = date.toLocaleDateString('en-US', { weekday: 'long' });
+            const weatherIconUrl = `https://openweathermap.org/img/wn/${forecastData.weather[0].icon}@2x.png`
 
-    let html = '';
 
-    for (let i = 0; i < 5; i++) {
-        const forecastData = list.list[i * 8];
-        // CONVERT TIMESTAP INTO DATE
-        const date = new Date(forecastData.dt * 1000);
-        // Multiply by 1000 on line 82 IOT convert seconds to millisconds
 
-        // Get day of week as a string
-        const dayOfWeek = date.toLocaleDateString('en-US', {weekday: 'long'})
+            // Each Forecast Card
+            html +=
+                ` <div className="card" style="width: 18rem;">
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">${dayOfWeek}</li>
+                    <li className="list-group-item">Main: ${forecastData.main.temp.toFixed(0)}°F</li>
+                    <li class="list-group-item">Low: ${forecastData.main.temp_min}°F</li>
+                    <li className="list-group-item">${forecastData.weather[0].main}</li>
+                    <li className="list-group-item"><img src="${weatherIconUrl}" alt="weather-icon"></li>
+                </ul>
+            </div>`;
+        }
 
-        // Each Forecast Card
-        html +=
-            `<div class="forecast-div">
-        <div class="day-week">${dayOfWeek}</div>
-        <img id="weathericon" alt=""/>
-        <hr>
-        <div>Main: ${forecastData.main.temp.toFixed(0)}°F</div>
-        <hr>
-        <div>Low: ${forecastData.main.temp_min}°F</div>
-        <br>
-        <div>${forecastData.weather[0].main}</div>
-        </div>`
-    }
-    // Insert to cards
-    $("#insert-forecast").html(html);
+        // Insert to #insert-forecast element
+        document.getElementById('insert-forecast').innerHTML = html;
+    })
+    .catch(error => {
+        console.error('Error fetching forecast data:', error);
+    });
 // ***************************************************************************
 // ***************************************************************************
 
     // SEATTLE CURRENT FORECAST-TOP CARD
     function updateCurrentCityForecast(data) {
+        console.log(data)
+        let iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
         let html = `
+<div class="city-forecast">
 <div class="city-name">${data.name}</div>
-<div>Current Temp:</div> 
-<div>${data.main.temp.toFixed(0)}°F</div>
-<div>Humidity:</div>
-<div>${data.main.humidity}%</div>
-<div>Conditions:</div> 
-<div class="cloud-img">${data.weather[0].main}</div>`;
+<br>
+<div class="city-temperature">${data.main.temp.toFixed(0)}°F</div>
+<div class="city-image d-flex"><img src="${iconUrl}" alt="Weather Icon"></div>
 
-        let backgroundImageURL = '';
+<!--<div class="cloud-img">${data.weather[0].icon}</div>-->
+</div>`;
+
+        let iconImageURL = '';
 
         if (data.weather[0].main === 'Clear' && data.main.temp > 70) {
-            backgroundImageURL = 'url("img/pexels-zszen-john-2651796.jpg")';
+            iconImageURL = 'url(" https://openweathermap.org/img/wn/01d@2x.png")';
             $("#insert-weather").css('color', 'black');
         } else if (data.weather[0].main === 'Clear' && data.main.temp <= 70) {
-            backgroundImageURL = 'url("img/pexels-zszen-john-2651796.jpg")';
+            iconImageURL = 'url(" https://openweathermap.org/img/wn/02d@2x.png")';
         } else if (data.weather[0].main === 'Rain') {
-            backgroundImageURL = 'url("img/pexels-valeriia-miller-2530911.jpg")';
+            iconImageURL = 'url("https://openweathermap.org/img/wn/11d@2x.png")';
         } else if (data.weather[0].main === 'Clouds') {
-            backgroundImageURL = 'url("img/pexels-alex-conchillos-3888585.jpg")';
+            iconImageURL = 'url("https://openweathermap.org/img/wn/03d@2x.png")';
         } else {
-            backgroundImageURL = 'url("img/pexels-stephan-seeber-1261728.jpg")';
+            iconImageURL = 'url("https://openweathermap.org/img/wn/11d@2x.png")';
         }
 
         // Apply the background image to my card
-        $("#insert-weather-img").css('background-image', backgroundImageURL).css('background-repeat', 'no-repeat');
+        $("#insert-weather-img").innerText;
 
         $("#insert-weather").html(html);
     }
@@ -205,7 +187,9 @@ $.get(BASE_FORECAST_URL + `lat=${47.60537214369371}&lon=${-122.32423484983421}&a
             updateCurrentCityForecast(data);
         });
     }
-// // Call the function to update the current city weather when the page loads
+// Call the function to update the current city weather when the page loads
     fetchAndUpdateCurrentCityWeather();
-})
+
+
+
 
